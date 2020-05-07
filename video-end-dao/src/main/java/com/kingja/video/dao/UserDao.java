86af65413fa33121052cece085b3af4a3789a2a6
KingJA <kingjavip@gmail.com>
2020-05-07 17:08:36 +0800

    add swaggger

--- video-end-dao/src/main/java/com/kingja/video/dao/UserDao.java
+++ video-end-dao/src/main/java/com/kingja/video/dao/UserDao.java
@@ -1,6 +1,9 @@
 package com.kingja.video.dao;
 
 
+import com.kingja.video.dao.dto.User;
+
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -15,4 +18,7 @@ import org.apache.ibatis.annotations.Select;
 public interface UserDao {
     @Select("select count(*) from users where username = #{username}")
     public int getCountByUserName(@Param("username") String username);
+
+    @Insert("insert into users (id,username, password)values(#{id},#{username}, #{password})")
+    public int insertUser(User user);
 }
